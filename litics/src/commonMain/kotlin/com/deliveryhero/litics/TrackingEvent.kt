afa0fc245612a1752ccdbd436fe2c694a80177ed
Veyndan Stuart <veyndan@gmail.com>
2021-11-12 15:25:39 +0100

    Make TrackingEvent#parameters non-nullable

--- litics/src/commonMain/kotlin/com/deliveryhero/litics/TrackingEvent.kt
+++ litics/src/commonMain/kotlin/com/deliveryhero/litics/TrackingEvent.kt
@@ -2,5 +2,5 @@ package com.deliveryhero.litics
 
 data class TrackingEvent(
     val eventName: String,
-    val parameters: Map<String, String>? = null,
+    val parameters: Map<String, String>,
 )
